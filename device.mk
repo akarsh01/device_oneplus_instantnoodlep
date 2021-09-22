8aa47d6b882b996933a33395acf1dffc029b8002
Magicxavi <rolfpeterkesche@gmail.com>
2021-09-22 03:53:56 +0300

    Lemonkebab: define APNs-Conf within tree

--- device.mk
+++ device.mk
@@ -27,6 +27,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aosp
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+    
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
